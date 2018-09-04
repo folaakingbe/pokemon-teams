59ffa044fbb78e9e9d9cc8ed8a6304382382ccfe
Fola <folaakingbe@yahoo.com>
2018-09-04 19:34:19 -0400

    Basic Structure

---
+++ src/components/Game.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Game = () => (
+    <div>
+        <h1>Pokemon Game</h1>
+        <h2>Name</h2>
+        <h2>ID</h2>
+        <h2>Team</h2>
+    </div>
+);
+
+export default Game;
