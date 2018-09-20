823c86891cb3d39a9d420f30fa66b92dc1ebf577
Fola <folaakingbe@yahoo.com>
2018-09-19 22:55:24 -0400

    Little Changes

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Game = (props) => (
-    <div style={props.style}>
+const Game = (props) => {
+    console.log(props.team.length);
+    return <div style={props.style}>
         <h1>{props.name}</h1>
-        <h2>{props.trainer}</h2>
-        <h2>{props.ID}</h2>
-        <h2>{props.team}</h2>
-    </div>
-);
+        <h2>Name: {props.trainer}</h2>
+        <h2>ID: {props.ID}</h2>
+        <h2>Team: {props.team.join(' ')}</h2>
+    </div>;
+};
 
 export default Game;
