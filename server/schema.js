9b94d399a51faa939eafd7e67fca53fce8681dca
Fola <folaakingbe@yahoo.com>
2018-10-03 20:13:33 -0400

    schema

---
+++ server/schema.js
@@ -0,0 +1,65 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = require('graphql');
+
+// Hardcoded Data
+
+const games = [
+    {id:'1', title:'Ruby', name:'Brendan', number:65530},
+    {id:'2', title:'Colosseum', name:'Wes', number:17041}
+];
+
+// Pokemon Type
+const PokemonType = new GraphQLObjectType({
+    name: 'Pokemon',
+    fields:() => ({
+        name: {type: GraphQLString},
+        nickname: {type: GraphQLString},
+        gender: {type: GraphQLString},
+        type1: {type: GraphQLString},
+        type2: {type: GraphQLString},
+        nature: {type: GraphQLString},
+        pokeball: {type: GraphQLString}
+    })
+});
+
+// Game Type
+const GameType = new GraphQLObjectType({
+    name: 'Game',
+    fields:() => ({
+        id: {type:GraphQLString},
+        title: {type: GraphQLString},
+        name: {type: GraphQLString},
+        number: {type: GraphQLInt}
+    })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields:{
+        game: {
+            type: GameType,
+            args:{
+                id:{type: GraphQLString}
+            },
+            resolve(parentValue, args){
+                for (let i = 0; i < games.length; i++){
+                    if (games[i].id == args.id) {
+                        return games[i];
+                    }
+                }
+            }
+        }
+    }
+    
+})
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+});
