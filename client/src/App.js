3b98ab6cfa5ed1a8b4f52d86dfcb35fb059c8061
Fola <folaakingbe@yahoo.com>
2019-01-14 02:18:50 -0500

    Added react-apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import './App.css';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Pokemon Teams</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <h1>Pokemon Teams</h1>
+        </div>
+      </ApolloProvider>
     );
   }
 }
